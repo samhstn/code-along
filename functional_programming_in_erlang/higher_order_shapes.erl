e9ea883857cb246d3fdbe2672037bff0b4da4241
samhstn <shouston3@me.com>
2020-07-10 18:24:27 +0100

    additional higher_order_functions implementation

--- functional_programming_in_erlang/higher_order_shapes.erl
+++ functional_programming_in_erlang/higher_order_shapes.erl
@@ -1,5 +1,5 @@
 -module(higher_order_shapes).
--export([all_areas/1,circles/1,sum/1]).
+-export([all_areas/1,circles/1,sum/1,doubleAll/1,evens/1,product/1,zip/2,zip_with/3]).
 
 map(_F,[]) -> [];
 map(F,[X|Xs]) -> [F(X)|map(F,Xs)].
@@ -11,6 +11,11 @@ filter(P,[X|Xs]) ->
     false -> filter(P,Xs)
   end.
 
+reduce(_Combine, Start, []) ->
+  Start;
+reduce(Combine, Start, [X|Xs]) ->
+  Combine(X, reduce(Combine, Start, Xs)).
+
 all_areas(Shapes) -> map(fun assignment:area/1, Shapes).
 
 is_circle({circle,{_,_},_}) -> true;
@@ -18,11 +23,16 @@ is_circle({rectangle,{_,_},_,_}) -> false.
 
 circles(Shapes) -> filter(fun is_circle/1,Shapes).
 
-reduce(_Combine, Start, []) ->
-  Start;
-reduce(Combine, Start, [X|Xs]) ->
-  Combine(X, reduce(Combine, Start, Xs)).
-
 sum(Xs) -> reduce(fun plus/2, 0, Xs).
 
 plus(X,Y) -> X+Y.
+
+doubleAll(Xs) -> map(fun(X) -> X*2 end, Xs).
+evens(Xs) -> filter(fun(X) -> X rem 2 == 0 end, Xs).
+product(Xs) -> reduce(fun(X,Y) -> X*Y end, 1, Xs).
+
+zip([X|Xs], [Y|Ys]) -> [{X,Y}|zip(Xs,Ys)];
+zip([],_Ys) -> [];
+zip(_Xs,[]) -> [].
+
+zip_with(F, Xs, Ys) -> lists:map(fun({X,Y}) -> F(X,Y) end, zip(Xs, Ys)).
