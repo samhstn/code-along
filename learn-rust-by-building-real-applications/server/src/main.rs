fc4bdb2a1d35eb535937d245074cb0fb4ec01eca
samhstn <shouston3@me.com>
2020-11-29 16:41:14 +0000

    separate out in to modules - rust fundamentals

--- learn-rust-by-building-real-applications/server/src/main.rs
+++ learn-rust-by-building-real-applications/server/src/main.rs
@@ -1,35 +1,48 @@
+use server::Server;
+use http::request::Request;
+
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
 
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self { // can also be Server instead of Self
-        Self { addr }
+mod server {
+    pub struct Server {
+        addr: String,
     }
 
-    fn run(self) {
-        println!("Listening on {}", self.addr);
+    impl Server {
+        pub fn new(addr: String) -> Self { // can also be Server instead of Self
+            Self { addr }
+        }
+
+        pub fn run(self) {
+            println!("Listening on {}", self.addr);
+        }
     }
 }
 
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method
-}
+mod http {
+    pub mod request {
+        use super::method::Method;
 
-enum Method {
-    GET,
-    POST,
-    PUT,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH
+        pub struct Request {
+            path: String,
+            query_string: Option<String>,
+            method: Method
+        }
+    }
+
+    mod method {
+        pub enum Method {
+            GET,
+            POST,
+            PUT,
+            HEAD,
+            CONNECT,
+            OPTIONS,
+            TRACE,
+            PATCH
+        }
+    }
 }
