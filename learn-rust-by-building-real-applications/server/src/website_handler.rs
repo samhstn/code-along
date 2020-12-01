b1f13888d20da1b09e976d62dbf417adcb091560
samhstn <shouston3@me.com>
2020-12-01 13:29:01 +0000

    implement file traversal security measure - rust fundamentals

--- learn-rust-by-building-real-applications/server/src/website_handler.rs
+++ learn-rust-by-building-real-applications/server/src/website_handler.rs
@@ -13,7 +13,17 @@ impl WebsiteHandler {
     
     fn read_file(&self, file_path: &str) -> Option<String> {
         let path = format!("{}/{}", self.public_path, file_path);
-        fs::read_to_string(path).ok()
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory Traversal Attack Attempted: {}", file_path);
+                    None
+                }
+            },
+            Err(_) => None,
+        }
     }
 }
 
